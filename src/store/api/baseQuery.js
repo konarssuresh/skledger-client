0219c7a02a07e51d22c16f4d1b42686657eaa198
Suresh Konar <konarssuresh@gmail.com>
2026-08-02 14:17:33 +0530

    updated netlify.toml and base query for iphone support

--- src/store/api/baseQuery.js
+++ src/store/api/baseQuery.js
@@ -1,7 +1,10 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// for local development add it in baseurl
+// import.meta.env.VITE_API_BASE_URL || "http://localhost:8000"
+
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
+  baseUrl: "",
   credentials: "include",
   prepareHeaders: (headers) => {
     headers.set("Content-Type", "application/json");
